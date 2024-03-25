87367669285995b8c5e417b6e0e1a3ce6d646f9a
JTMGaming <porkkanalaatikko2@gmail.com>
2024-03-25 13:22:57 +0200

    Update

--- TehtäväNippu2/tehtavanippu2/src/main/java/com/example/Main.java
+++ TehtäväNippu2/tehtavanippu2/src/main/java/com/example/Main.java
@@ -3,10 +3,14 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         Teh11 teh11 = new Teh11();
+        Teh12 teh12 = new Teh12();
 
+        //11
+        // System.out.println(teh11.big());
 
+        //12
+        // System.out.println(teh12.randomName());
 
-        //1
-        System.out.println(teh11.big());
+        
     }
 }
--- TehtäväNippu2/tehtavanippu2/src/main/java/com/example/Teh12.java
+++ TehtäväNippu2/tehtavanippu2/src/main/java/com/example/Teh12.java
@@ -1,5 +1,14 @@
 package com.example;
+import java.util.Random;
 
 public class Teh12 {
-    
+    public String randomName(){
+        Random rnd = new Random();
+        String[] etunimet = {"Juhani", "Olavi", "Tapani", "Aino", "Eeva", "Liisa", "Kaisa", "Matti", "Mikko", "Ilmari", "Risto", "Antti", "Oskari", "Teemu", "Sakari", "Tuomas", "Ville", "Jarmo", "Heikki", "Pekka"};
+        String[] sukunimet = {"Korhonen", "Virtanen", "Mäkinen", "Nieminen", "Mäkelä", "Hämäläinen", "Laine", "Heikkinen", "Koskinen", "Järvinen", "Lehtonen", "Lehtinen", "Saarinen", "Salminen", "Rantanen", "Jokinen", "Peltonen", "Tuominen", "Laaksonen", "Heinonen"};
+        int rInt = rnd.nextInt(20);
+        int gInt = rnd.nextInt(20);
+        String rName = etunimet[rInt] + " " + sukunimet[gInt];
+        return rName;
+    }
 }
